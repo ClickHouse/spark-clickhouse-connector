c484ca7c82ba938abdddfa1045e6535929a97c0d
Cheng Pan <chengpan@apache.org>
2021-08-24 21:04:24 +0800

    Support convert writing distributed table to local when field as sharding key

--- clickhouse-spark-32/src/main/scala/xenon/clickhouse/write/ClickHouseWriter.scala
+++ clickhouse-spark-32/src/main/scala/xenon/clickhouse/write/ClickHouseWriter.scala
@@ -21,13 +21,15 @@ import scala.util.{Failure, Success}
 
 import com.google.protobuf.ByteString
 import org.apache.spark.sql.catalyst.{InternalRow, SQLConfHelper}
+import org.apache.spark.sql.catalyst.expressions.{BoundReference, Expression, SafeProjection}
+import org.apache.spark.sql.clickhouse.{ExprUtils, JsonFormatUtils}
 import org.apache.spark.sql.clickhouse.ClickHouseSQLConf._
-import org.apache.spark.sql.clickhouse.JsonFormatUtils
 import org.apache.spark.sql.connector.write.{DataWriter, WriterCommitMessage}
+import org.apache.spark.sql.types.{ByteType, IntegerType, LongType, ShortType}
 import xenon.clickhouse._
 import xenon.clickhouse.exception._
 import xenon.clickhouse.grpc.{GrpcClusterClient, GrpcNodeClient}
-import xenon.clickhouse.spec.DistributedEngineSpec
+import xenon.clickhouse.spec.{DistributedEngineSpec, ShardUtils}
 
 class ClickHouseAppendWriter(writeJob: WriteJobDescription)
     extends DataWriter[InternalRow] with SQLConfHelper with Logging {
@@ -38,6 +40,27 @@ class ClickHouseAppendWriter(writeJob: WriteJobDescription)
 
   val database: String = writeJob.targetDatabase(writeDistributedConvertLocal)
   val table: String = writeJob.targetTable(writeDistributedConvertLocal)
+
+  private lazy val shardExpr: Option[Expression] = writeJob.sparkShardExpr match {
+    case None if writeJob.tableEngineSpec.is_distributed =>
+      throw ClickHouseClientException("Can not write data to a Distributed table that lacks sharding key")
+    case None => None
+    case Some(v2Expr) =>
+      val catalystExpr = ExprUtils.toCatalyst(v2Expr, writeJob.dataSetSchema.fields)
+      catalystExpr match {
+        case BoundReference(_, dataType, _)
+            if dataType.isInstanceOf[ByteType] // list all integral types here because we can not access `IntegralType`
+              || dataType.isInstanceOf[ShortType]
+              || dataType.isInstanceOf[IntegerType]
+              || dataType.isInstanceOf[LongType] =>
+          Some(catalystExpr)
+        case BoundReference(_, dataType, _) =>
+          throw ClickHouseClientException(s"Invalid data type of sharding field: $dataType")
+        case unsupported: Expression =>
+          log.warn(s"Unsupported expression of sharding field: $unsupported")
+          None
+      }
+  }
 
   // put the node select strategy in executor side because we need to calculate shard and don't know the records
   // util DataWriter#write(InternalRow) invoked.
@@ -57,21 +80,24 @@ class ClickHouseAppendWriter(writeJob: WriteJobDescription)
         Right(GrpcNodeClient(nodeSpec))
     }
 
-  def grpcNodeClient: GrpcNodeClient = grpcClient match {
-    case Left(clusterClient) => clusterClient.node()
+  def grpcNodeClient(shardNum: Option[Int]): GrpcNodeClient = grpcClient match {
+    case Left(clusterClient) => clusterClient.node(shardNum)
     case Right(nodeClient) => nodeClient
   }
 
+  var lastShardNum: Option[Int] = None
   val buf: ArrayBuffer[ByteString] = new ArrayBuffer[ByteString](batchSize)
 
   override def write(record: InternalRow): Unit = {
-    // TODO evaluate shard, flush if shard change or reach batchSize
+    val shardNum = calcShard(record)
+    if (shardNum != lastShardNum && buf.nonEmpty) flush(lastShardNum)
+    lastShardNum = shardNum
     buf += JsonFormatUtils.row2Json(record, writeJob.dataSetSchema, writeJob.tz)
-    if (buf.size == batchSize) flush()
+    if (buf.size == batchSize) flush(lastShardNum)
   }
 
   override def commit(): WriterCommitMessage = {
-    if (buf.nonEmpty) flush()
+    if (buf.nonEmpty) flush(lastShardNum)
     CommitMessage(s"Job[${writeJob.queryId}]: commit")
   }
 
@@ -82,19 +108,48 @@ class ClickHouseAppendWriter(writeJob: WriteJobDescription)
     case Right(nodeClient) => nodeClient.close()
   }
 
-  def flush(): Unit =
+  def calcShard(record: InternalRow): Option[Int] =
+    if (!writeDistributedConvertLocal) None
+    else shardExpr match {
+      case Some(expr @ BoundReference(_, dataType, _)) =>
+        val shardProj = SafeProjection.create(Seq(expr))
+        val resultRow = shardProj(record)
+        val shardValue = dataType match {
+          case ByteType => resultRow.getByte(0).toLong
+          case ShortType => resultRow.getShort(0).toLong
+          case IntegerType => resultRow.getInt(0).toLong
+          case LongType => resultRow.getLong(0)
+        }
+        val shardSpec = ShardUtils.calcShard(writeJob.cluster.get, shardValue)
+        Some(shardSpec.num)
+      case _ => None
+    }
+
+  def flush(shardNum: Option[Int]): Unit =
     Utils.retry[Unit, RetryableClickHouseException](
       conf.getConf(WRITE_MAX_RETRY),
       Duration.ofSeconds(conf.getConf(WRITE_RETRY_INTERVAL))
     ) {
-      grpcNodeClient.syncInsert(database, table, "JSONEachRow", buf.reduce((l, r) => l concat r)) match {
+      val client = grpcNodeClient(shardNum)
+      log.info(s"""Job[${writeJob.queryId}]: prepare to flush batch
+                  |node: ${client.node.host}:${client.node.grpc_port}
+                  |batch size: ${buf.size}
+                  |cluster: ${writeJob.cluster.map(_.name)}
+                  |shard: $shardNum
+                  |""".stripMargin)
+      client.syncInsert(
+        database,
+        table,
+        "JSONEachRow",
+        buf.reduce((l, r) => l concat r)
+      ) match {
         case Right(_) => buf.clear
         case Left(retryable) if conf.getConf(WRITE_RETRYABLE_ERROR_CODES).contains(retryable.getCode) =>
           throw new RetryableClickHouseException(retryable)
         case Left(rethrow) => throw new ClickHouseServerException(rethrow)
       }
     } match {
-      case Success(_) => log.info(s"Job[${writeJob.queryId}]: flush batch")
+      case Success(_) => log.info(s"Job[${writeJob.queryId}]: flush batch completed")
       case Failure(rethrow) => throw rethrow
     }
 }
