7fe78add935820918e6a1e9d62889e2dcb367a80
Cheng Pan <chengpan@apache.org>
2022-05-28 07:30:58 +0800

    Spark 3.3: Add conf spark.clickhouse.write.repartitionByPartition

--- spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/single/WriteDistributionAndOrderingSuite.scala
+++ spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/single/WriteDistributionAndOrderingSuite.scala
@@ -25,7 +25,7 @@ import org.apache.spark.sql.clickhouse.ClickHouseSQLConf._
 import xenon.clickhouse.Logging
 import xenon.clickhouse.base.ClickHouseSingleMixIn
 
-class LocalSortByKeySuite extends BaseSparkSuite
+class WriteDistributionAndOrderingSuite extends BaseSparkSuite
     with ClickHouseSingleMixIn
     with SparkClickHouseSingleMixin
     with SparkClickHouseSingleTestHelper
@@ -33,8 +33,51 @@ class LocalSortByKeySuite extends BaseSparkSuite
 
   import spark.implicits._
 
+  test("write repartitionByPartition") {
+    autoCleanupTable("db_repartitionByPartition", "tbl_repartitionByPartition") { (db, tbl) =>
+      runClickHouseSQL(
+        s"""CREATE TABLE `$db`.`$tbl` (
+           |    `id` String,
+           |    `load_date` Date
+           |) ENGINE = MergeTree
+           |ORDER BY load_date
+           |PARTITION BY xxHash64(id)
+           |""".stripMargin
+      )
+      import org.apache.spark.sql.functions._
+
+      spark.sessionState.conf.setConf(WRITE_REPARTITION_BY_PARTITION, true)
+      // SPARK-39313
+      // intercept[AnalysisException] {
+      //   spark.range(3)
+      //     .toDF("id")
+      //     .withColumn("id", $"id".cast(StringType))
+      //     .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
+      //     .writeTo(s"$db.$tbl")
+      //     .append
+      // }
+      spark.sessionState.conf.setConf(WRITE_REPARTITION_BY_PARTITION, false)
+      spark.range(3)
+        .toDF("id")
+        .withColumn("id", $"id".cast(StringType))
+        .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
+        .writeTo(s"$db.$tbl")
+        .append
+
+      checkAnswer(
+        spark.sql(s"SELECT id, load_date FROM $db.$tbl"),
+        Seq(
+          Row("0", Date.valueOf("2022-05-27")),
+          Row("1", Date.valueOf("2022-05-27")),
+          Row("2", Date.valueOf("2022-05-27"))
+        )
+      )
+      spark.sessionState.conf.unsetConf(WRITE_REPARTITION_BY_PARTITION)
+    }
+  }
+
   test("write localSortByKey") {
-    autoCleanupTable("db_sort", "tbl_sort") { (db, tbl) =>
+    autoCleanupTable("db_localSortByKey", "tbl_localSortByKey") { (db, tbl) =>
       runClickHouseSQL(
         s"""CREATE TABLE `$db`.`$tbl` (
            |    `id` String,
@@ -42,7 +85,6 @@ class LocalSortByKeySuite extends BaseSparkSuite
            |) ENGINE = MergeTree
            |ORDER BY xxHash64(id)
            |PARTITION BY load_date
-           |SAMPLE BY xxHash64(id)
            |""".stripMargin
       )
       import org.apache.spark.sql.functions._
@@ -73,7 +115,7 @@ class LocalSortByKeySuite extends BaseSparkSuite
           Row("2", Date.valueOf("2022-05-27"))
         )
       )
-      spark.sessionState.conf.clear()
+      spark.sessionState.conf.unsetConf(WRITE_LOCAL_SORT_BY_KEY)
     }
   }
 }
