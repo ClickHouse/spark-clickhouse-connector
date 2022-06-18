372626c4734d0791b8e209c2cca5a61cb42fdc7d
Cheng Pan <chengpan@apache.org>
2022-06-18 14:53:48 +0800

    Spark 3.2: Simplify configuration

--- spark-3.2/clickhouse-spark/src/main/scala/org/apache/spark/sql/clickhouse/SparkOptions.scala
+++ spark-3.2/clickhouse-spark/src/main/scala/org/apache/spark/sql/clickhouse/SparkOptions.scala
@@ -14,31 +14,13 @@
 
 package org.apache.spark.sql.clickhouse
 
-import java.time.Duration
-import java.util.{Map => JMap}
-
 import org.apache.spark.internal.config.ConfigEntry
 import org.apache.spark.sql.catalyst.SQLConfHelper
 import org.apache.spark.sql.clickhouse.ClickHouseSQLConf._
-import org.apache.spark.sql.clickhouse.SparkOptions._
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 
-object SparkOptions {
-  val READ_DISTRIBUTED_USE_CLUSTER_NODES_KEY: String = "read.distributed.useClusterNodes"
-  val READ_DISTRIBUTED_CONVERT_LOCAL_KEY: String = "read.distributed.convertLocal"
-
-  val WRITE_BATCH_SIZE_KEY: String = "write.batchSize"
-  val WRITE_MAX_RETRY_KEY: String = "write.maxRetry"
-  val WRITE_RETRY_INTERVAL_KEY: String = "write.retryInterval"
-  val WRITE_RETRYABLE_ERROR_CODES_KEY: String = "write.retryableErrorCodes"
-  val WRITE_REPARTITION_NUM_KEY: String = "write.repartitionNum"
-  val WRITE_REPARTITION_BY_PARTITION_KEY: String = "write.repartitionByPartition"
-  val WRITE_DISTRIBUTED_USE_CLUSTER_NODES_KEY: String = "write.distributed.useClusterNodes"
-  val WRITE_DISTRIBUTED_CONVERT_LOCAL_KEY: String = "write.distributed.convertLocal"
-  val WRITE_LOCAL_SORT_BY_PARTITION_KEY: String = "write.localSortByPartition"
-  val WRITE_LOCAL_SORT_BY_KEY_KEY: String = "write.localSortByKey"
-  val WRITE_COMPRESSION_CODEC_KEY: String = "write.compression.codec"
-}
+import java.time.Duration
+import java.util.{Map => JMap}
 
 trait SparkOptions extends SQLConfHelper with Serializable {
   protected def options: CaseInsensitiveStringMap
@@ -52,42 +34,42 @@ class ReadOptions(_options: JMap[String, String]) extends SparkOptions {
   override protected def options: CaseInsensitiveStringMap = new CaseInsensitiveStringMap(_options)
 
   def useClusterNodesForDistributed: Boolean =
-    eval(READ_DISTRIBUTED_USE_CLUSTER_NODES_KEY, READ_DISTRIBUTED_USE_CLUSTER_NODES)
+    eval(READ_DISTRIBUTED_USE_CLUSTER_NODES.key, READ_DISTRIBUTED_USE_CLUSTER_NODES)
 
   def convertDistributedToLocal: Boolean =
-    eval(READ_DISTRIBUTED_CONVERT_LOCAL_KEY, READ_DISTRIBUTED_CONVERT_LOCAL)
+    eval(READ_DISTRIBUTED_CONVERT_LOCAL.key, READ_DISTRIBUTED_CONVERT_LOCAL)
 }
 
 class WriteOptions(_options: JMap[String, String]) extends SparkOptions {
 
   override protected def options: CaseInsensitiveStringMap = new CaseInsensitiveStringMap(_options)
 
-  def batchSize: Int = eval(WRITE_BATCH_SIZE_KEY, WRITE_BATCH_SIZE)
+  def batchSize: Int = eval(WRITE_BATCH_SIZE.key, WRITE_BATCH_SIZE)
 
-  def maxRetry: Int = eval(WRITE_MAX_RETRY_KEY, WRITE_MAX_RETRY)
+  def maxRetry: Int = eval(WRITE_MAX_RETRY.key, WRITE_MAX_RETRY)
 
   def retryInterval: Duration =
-    Duration.ofSeconds(eval(WRITE_RETRY_INTERVAL_KEY, WRITE_RETRY_INTERVAL))
+    Duration.ofSeconds(eval(WRITE_RETRY_INTERVAL.key, WRITE_RETRY_INTERVAL))
 
-  def retryableErrorCodes: Seq[Int] = eval(WRITE_RETRYABLE_ERROR_CODES_KEY, WRITE_RETRYABLE_ERROR_CODES)
+  def retryableErrorCodes: Seq[Int] = eval(WRITE_RETRYABLE_ERROR_CODES.key, WRITE_RETRYABLE_ERROR_CODES)
 
-  def repartitionNum: Int = eval(WRITE_REPARTITION_NUM_KEY, WRITE_REPARTITION_NUM)
+  def repartitionNum: Int = eval(WRITE_REPARTITION_NUM.key, WRITE_REPARTITION_NUM)
 
   def repartitionByPartition: Boolean =
-    eval(WRITE_REPARTITION_BY_PARTITION_KEY, WRITE_REPARTITION_BY_PARTITION)
+    eval(WRITE_REPARTITION_BY_PARTITION.key, WRITE_REPARTITION_BY_PARTITION)
 
   def useClusterNodesForDistributed: Boolean =
-    eval(WRITE_DISTRIBUTED_USE_CLUSTER_NODES_KEY, WRITE_DISTRIBUTED_USE_CLUSTER_NODES)
+    eval(WRITE_DISTRIBUTED_USE_CLUSTER_NODES.key, WRITE_DISTRIBUTED_USE_CLUSTER_NODES)
 
   def convertDistributedToLocal: Boolean =
-    eval(WRITE_DISTRIBUTED_CONVERT_LOCAL_KEY, WRITE_DISTRIBUTED_CONVERT_LOCAL)
+    eval(WRITE_DISTRIBUTED_CONVERT_LOCAL.key, WRITE_DISTRIBUTED_CONVERT_LOCAL)
 
   def localSortByPartition: Boolean =
-    eval(WRITE_LOCAL_SORT_BY_PARTITION_KEY, WRITE_LOCAL_SORT_BY_PARTITION)
+    eval(WRITE_LOCAL_SORT_BY_PARTITION.key, WRITE_LOCAL_SORT_BY_PARTITION)
 
   def localSortByKey: Boolean =
-    eval(WRITE_LOCAL_SORT_BY_KEY_KEY, WRITE_LOCAL_SORT_BY_KEY)
+    eval(WRITE_LOCAL_SORT_BY_KEY.key, WRITE_LOCAL_SORT_BY_KEY)
 
   def compressionCodec: Option[String] =
-    eval(WRITE_COMPRESSION_CODEC_KEY, WRITE_COMPRESSION_CODEC)
+    eval(WRITE_COMPRESSION_CODEC.key, WRITE_COMPRESSION_CODEC)
 }
