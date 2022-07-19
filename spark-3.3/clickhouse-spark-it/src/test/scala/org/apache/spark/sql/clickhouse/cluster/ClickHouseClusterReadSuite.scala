7e0ccae2fca90bd6a3a65c900821bacf3a265687
Cheng Pan <chengpan@apache.org>
2022-07-19 15:29:22 +0800

    Spark 3.3: Reorganize test suites

--- spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/cluster/ClickHouseClusterReadSuite.scala
+++ spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/cluster/ClickHouseClusterReadSuite.scala
@@ -14,45 +14,35 @@
 
 package org.apache.spark.sql.clickhouse.cluster
 
-import org.apache.spark.sql.{AnalysisException, Row}
-import org.apache.spark.sql.QueryTest._
-import org.apache.spark.sql.clickhouse.BaseSparkSuite
 import org.apache.spark.sql.clickhouse.ClickHouseSQLConf.READ_DISTRIBUTED_CONVERT_LOCAL
-import xenon.clickhouse.Logging
-import xenon.clickhouse.base.ClickHouseClusterMixIn
+import org.apache.spark.sql.{AnalysisException, Row}
 
-class ClickHouseClusterReadSuite extends BaseSparkSuite
-    with ClickHouseClusterMixIn
-    with SparkClickHouseClusterMixin
-    with SparkClickHouseClusterTestHelper
-    with Logging {
+class ClickHouseClusterReadSuite extends SparkClickHouseClusterTest {
 
   test("clickhouse metadata column - distributed table") {
     withSimpleDistTable("single_replica", "db_w", "t_dist", true) { (_, db, tbl_dist, _) =>
       assert(READ_DISTRIBUTED_CONVERT_LOCAL.defaultValueString == "true")
 
-      // `_shard_num` is dedicated for Distributed table
-      val cause = intercept[AnalysisException] {
-        spark.sql(s"SELECT y, _shard_num FROM $db.$tbl_dist")
+      withSQLConf(READ_DISTRIBUTED_CONVERT_LOCAL.key -> "true") {
+        // `_shard_num` is dedicated for Distributed table
+        val cause = intercept[AnalysisException] {
+          spark.sql(s"SELECT y, _shard_num FROM $db.$tbl_dist")
+        }
+        assert(cause.message.contains("cannot resolve '_shard_num' given input columns") ||
+          cause.message.contains("Column '_shard_num' does not exist"))
       }
-      assert(cause.message.contains("cannot resolve '_shard_num' given input columns") ||
-        cause.message.contains("Column '_shard_num' does not exist"))
 
-      spark.sql(s"SET ${READ_DISTRIBUTED_CONVERT_LOCAL.key}=false")
-      checkAnswer(
-        spark.sql(s"SELECT y, _shard_num FROM $db.$tbl_dist"),
-        Seq(
-          Row(2021, 2),
-          Row(2022, 3),
-          Row(2023, 4),
-          Row(2024, 1)
+      withSQLConf(READ_DISTRIBUTED_CONVERT_LOCAL.key -> "false") {
+        checkAnswer(
+          spark.sql(s"SELECT y, _shard_num FROM $db.$tbl_dist"),
+          Seq(
+            Row(2021, 2),
+            Row(2022, 3),
+            Row(2023, 4),
+            Row(2024, 1)
+          )
         )
-      )
-
-      // reset
-      spark.sql(s"SET ${READ_DISTRIBUTED_CONVERT_LOCAL.key}=true")
-
-    // infiniteLoop()
+      }
     }
   }
 
--- spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/single/WriteDistributionAndOrderingSuite.scala
+++ spark-3.3/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/single/WriteDistributionAndOrderingSuite.scala
@@ -14,23 +14,17 @@
 
 package org.apache.spark.sql.clickhouse.single
 
+import org.apache.spark.sql.clickhouse.ClickHouseSQLConf._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.{AnalysisException, Row}
+
 import java.sql.Date
 import java.time.LocalDate
-import org.apache.spark.sql.{AnalysisException, Row}
-import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.QueryTest.checkAnswer
-import org.apache.spark.sql.clickhouse.BaseSparkSuite
-import org.apache.spark.sql.clickhouse.ClickHouseSQLConf._
-import xenon.clickhouse.Logging
-import xenon.clickhouse.base.ClickHouseSingleMixIn
 
-class WriteDistributionAndOrderingSuite extends BaseSparkSuite
-    with ClickHouseSingleMixIn
-    with SparkClickHouseSingleMixin
-    with SparkClickHouseSingleTestHelper
-    with Logging {
+class WriteDistributionAndOrderingSuite extends SparkClickHouseSingleTest {
 
-  import spark.implicits._
+  import testImplicits._
 
   test("write repartitionByPartition") {
     autoCleanupTable("db_repartitionByPartition", "tbl_repartitionByPartition") { (db, tbl) =>
@@ -45,33 +39,35 @@ class WriteDistributionAndOrderingSuite extends BaseSparkSuite
       )
       import org.apache.spark.sql.functions._
 
-      spark.sessionState.conf.setConf(WRITE_REPARTITION_BY_PARTITION, true)
-      intercept[AnalysisException] {
+      withSQLConf(WRITE_REPARTITION_BY_PARTITION.key -> "true") {
+        intercept[AnalysisException] {
+          spark.range(3)
+            .toDF("id")
+            .withColumn("id", $"id".cast(StringType))
+            .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
+            .writeTo(s"$db.$tbl")
+            .append
+        }
+      }
+
+      withSQLConf(WRITE_REPARTITION_BY_PARTITION.key -> "false") {
         spark.range(3)
           .toDF("id")
           .withColumn("id", $"id".cast(StringType))
           .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
           .writeTo(s"$db.$tbl")
           .append
-      }
 
-      spark.sessionState.conf.setConf(WRITE_REPARTITION_BY_PARTITION, false)
-      spark.range(3)
-        .toDF("id")
-        .withColumn("id", $"id".cast(StringType))
-        .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
-        .writeTo(s"$db.$tbl")
-        .append
-
-      checkAnswer(
-        spark.sql(s"SELECT id, load_date FROM $db.$tbl"),
-        Seq(
-          Row("0", Date.valueOf("2022-05-27")),
-          Row("1", Date.valueOf("2022-05-27")),
-          Row("2", Date.valueOf("2022-05-27"))
+        checkAnswer(
+          spark.sql(s"SELECT id, load_date FROM $db.$tbl"),
+          Seq(
+            Row("0", Date.valueOf("2022-05-27")),
+            Row("1", Date.valueOf("2022-05-27")),
+            Row("2", Date.valueOf("2022-05-27"))
+          )
         )
-      )
-      spark.sessionState.conf.unsetConf(WRITE_REPARTITION_BY_PARTITION)
+        spark.sessionState.conf.unsetConf(WRITE_REPARTITION_BY_PARTITION)
+      }
     }
   }
 
@@ -86,35 +82,35 @@ class WriteDistributionAndOrderingSuite extends BaseSparkSuite
            |PARTITION BY load_date
            |""".stripMargin
       )
-      import org.apache.spark.sql.functions._
 
-      spark.sessionState.conf.setConf(WRITE_LOCAL_SORT_BY_KEY, true)
-      intercept[AnalysisException] {
+      withSQLConf(WRITE_LOCAL_SORT_BY_KEY.key -> "true") {
+        intercept[AnalysisException] {
+          spark.range(3)
+            .toDF("id")
+            .withColumn("id", $"id".cast(StringType))
+            .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
+            .writeTo(s"$db.$tbl")
+            .append
+        }
+      }
+
+      withSQLConf(WRITE_LOCAL_SORT_BY_KEY.key -> "false") {
         spark.range(3)
           .toDF("id")
           .withColumn("id", $"id".cast(StringType))
           .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
           .writeTo(s"$db.$tbl")
           .append
-      }
 
-      spark.sessionState.conf.setConf(WRITE_LOCAL_SORT_BY_KEY, false)
-      spark.range(3)
-        .toDF("id")
-        .withColumn("id", $"id".cast(StringType))
-        .withColumn("load_date", lit(LocalDate.of(2022, 5, 27)))
-        .writeTo(s"$db.$tbl")
-        .append
-
-      checkAnswer(
-        spark.sql(s"SELECT id, load_date FROM $db.$tbl"),
-        Seq(
-          Row("0", Date.valueOf("2022-05-27")),
-          Row("1", Date.valueOf("2022-05-27")),
-          Row("2", Date.valueOf("2022-05-27"))
+        checkAnswer(
+          spark.sql(s"SELECT id, load_date FROM $db.$tbl"),
+          Seq(
+            Row("0", Date.valueOf("2022-05-27")),
+            Row("1", Date.valueOf("2022-05-27")),
+            Row("2", Date.valueOf("2022-05-27"))
+          )
         )
-      )
-      spark.sessionState.conf.unsetConf(WRITE_LOCAL_SORT_BY_KEY)
+      }
     }
   }
 }
