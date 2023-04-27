63c9dedd1b0a23c00ca15c1a9979b301d88f7f14
Cheng Pan <chengpan@apache.org>
2023-04-27 13:05:33 +0800

    Spark 3.3: Fix custom options (#234)

---
+++ spark-3.3/clickhouse-spark/src/test/scala/org/apache/spark/sql/clickhouse/ClickHouseHelperSuite.scala
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.clickhouse
+
+import org.apache.spark.sql.util.CaseInsensitiveStringMap
+import org.scalatest.funsuite.AnyFunSuite
+import xenon.clickhouse.ClickHouseHelper
+
+import scala.collection.JavaConverters._
+
+class ClickHouseHelperSuite extends AnyFunSuite with ClickHouseHelper {
+
+  test("buildNodeSpec") {
+    val nodeSpec = buildNodeSpec(
+      new CaseInsensitiveStringMap(Map(
+        "database" -> "testing",
+        "option.database" -> "production",
+        "option.ssl" -> "true"
+      ).asJava)
+    )
+    assert(nodeSpec.database === "testing")
+    assert(nodeSpec.options.get("ssl") === "true")
+  }
+}
