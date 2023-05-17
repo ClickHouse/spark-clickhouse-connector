2f46a1ccd8f4e3a69f24fd4a39f4cd8769de1308
Xinyuan Yang <yangxinyuan100@gmail.com>
2023-05-17 14:54:38 +0800

    Spark 3.4: Enhance FunctionRegistry to support some hash functions

---
+++ spark-3.4/clickhouse-spark-it/src/test/scala/org/apache/spark/sql/clickhouse/cluster/ClickHouseClusterHashUDFSuite.scala
@@ -0,0 +1,103 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.clickhouse.cluster
+
+import org.apache.spark.sql.clickhouse.TestUtils.om
+import xenon.clickhouse.func._
+import java.lang.{Long => JLong}
+
+class ClickHouseClusterHashUDFSuite extends SparkClickHouseClusterTest {
+  // only for query function names
+  val dummyRegistry: CompositeFunctionRegistry = {
+    val dynamicFunctionRegistry = new DynamicFunctionRegistry
+    val xxHash64ShardFunc = new ClickHouseXxHash64Shard(Seq.empty)
+    dynamicFunctionRegistry.register("ck_xx_hash64_shard", xxHash64ShardFunc) // for compatible
+    dynamicFunctionRegistry.register("clickhouse_shard_xxHash64", xxHash64ShardFunc)
+    new CompositeFunctionRegistry(Array(StaticFunctionRegistry, dynamicFunctionRegistry))
+  }
+
+  def runTest(funcSparkName: String, funcCkName: String, stringVal: String): Unit = {
+    val sparkResult = spark.sql(
+      s"""SELECT
+         |  $funcSparkName($stringVal)                         AS hash_value
+         |""".stripMargin
+    ).collect
+    assert(sparkResult.length == 1)
+    val sparkHashVal = sparkResult.head.getAs[Long]("hash_value")
+
+    val clickhouseResultJsonStr = runClickHouseSQL(
+      s"""SELECT
+         |  $funcCkName($stringVal)     AS hash_value
+         |""".stripMargin
+    ).head.getString(0)
+    val clickhouseResultJson = om.readTree(clickhouseResultJsonStr)
+    val clickhouseHashVal = JLong.parseUnsignedLong(clickhouseResultJson.get("hash_value").asText)
+    assert(
+      sparkHashVal == clickhouseHashVal,
+      s"ck_function: $funcCkName, spark_function: $funcSparkName, args: ($stringVal)"
+    )
+  }
+
+  Seq(
+    "clickhouse_xxHash64",
+    "clickhouse_murmurHash3_64",
+    "clickhouse_murmurHash3_32",
+    "clickhouse_murmurHash2_64",
+    "clickhouse_murmurHash2_32",
+    "clickhouse_cityHash64"
+  ).foreach { funcSparkName =>
+    val funcCkName = dummyRegistry.sparkToClickHouseFunc(funcSparkName)
+    test(s"UDF $funcSparkName") {
+      Seq(
+        "spark-clickhouse-connector",
+        "Apache Spark",
+        "ClickHouse",
+        "Yandex",
+        "热爱",
+        "在传统的行式数据库系统中，数据按如下顺序存储：",
+        "🇨🇳"
+      ).foreach { rawStringVal =>
+        val stringVal = s"\'$rawStringVal\'"
+        runTest(funcSparkName, funcCkName, stringVal)
+      }
+    }
+  }
+
+  Seq(
+    "clickhouse_murmurHash3_64",
+    "clickhouse_murmurHash3_32",
+    "clickhouse_murmurHash2_64",
+    "clickhouse_murmurHash2_32",
+    "clickhouse_cityHash64"
+  ).foreach { funcSparkName =>
+    val funcCkName = dummyRegistry.sparkToClickHouseFunc(funcSparkName)
+    test(s"UDF $funcSparkName multiple args") {
+      val strings = Seq(
+        "\'spark-clickhouse-connector\'",
+        "\'Apache Spark\'",
+        "\'ClickHouse\'",
+        "\'Yandex\'",
+        "\'热爱\'",
+        "\'在传统的行式数据库系统中，数据按如下顺序存储：\'",
+        "\'🇨🇳\'"
+      )
+      val test_5 = strings.combinations(5)
+      test_5.foreach { seq =>
+        val stringVal = seq.mkString(", ")
+        runTest(funcSparkName, funcCkName, stringVal)
+      }
+    }
+  }
+}
---
+++ spark-3.4/clickhouse-spark/src/test/scala/org/apache/spark/sql/clickhouse/FunctionRegistrySuite.scala
@@ -0,0 +1,55 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.clickhouse
+
+import org.scalatest.funsuite.AnyFunSuite
+import xenon.clickhouse.func._
+
+class FunctionRegistrySuite extends AnyFunSuite {
+
+  val staticFunctionRegistry: StaticFunctionRegistry.type = StaticFunctionRegistry
+  val dynamicFunctionRegistry = new DynamicFunctionRegistry
+  dynamicFunctionRegistry.register("ck_xx_hash64", ClickHouseXxHash64)
+  dynamicFunctionRegistry.register("clickhouse_xxHash64", ClickHouseXxHash64)
+
+  test("check StaticFunctionRegistry mappings") {
+    assert(staticFunctionRegistry.sparkToClickHouseFunc.forall { case (k, v) =>
+      staticFunctionRegistry.load(k).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(v)
+    })
+    assert(staticFunctionRegistry.clickHouseToSparkFunc.forall { case (k, v) =>
+      staticFunctionRegistry.load(v).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(k)
+    })
+  }
+
+  test("check DynamicFunctionRegistry mappings") {
+    assert(dynamicFunctionRegistry.sparkToClickHouseFunc.forall { case (k, v) =>
+      dynamicFunctionRegistry.load(k).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(v)
+    })
+    assert(dynamicFunctionRegistry.clickHouseToSparkFunc.forall { case (k, v) =>
+      dynamicFunctionRegistry.load(v).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(k)
+    })
+  }
+
+  test("check CompositeFunctionRegistry mappings") {
+    val compositeFunctionRegistry =
+      new CompositeFunctionRegistry(Array(staticFunctionRegistry, dynamicFunctionRegistry))
+    assert(compositeFunctionRegistry.sparkToClickHouseFunc.forall { case (k, v) =>
+      compositeFunctionRegistry.load(k).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(v)
+    })
+    assert(compositeFunctionRegistry.clickHouseToSparkFunc.forall { case (k, v) =>
+      compositeFunctionRegistry.load(v).get.asInstanceOf[ClickhouseEquivFunction].ckFuncNames.contains(k)
+    })
+  }
+}
