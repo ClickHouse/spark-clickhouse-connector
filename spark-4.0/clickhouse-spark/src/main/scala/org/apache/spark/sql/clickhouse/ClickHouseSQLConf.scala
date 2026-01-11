c18d044129576e80cb5a921b0d60231a0aaa8d35
ShimonSte <shimon.steinitz@clickhouse.com>
2026-01-11 15:18:54 +0200

    fix: avoid forward reference NPE in WRITE_DISTRIBUTED_CONVERT_LOCAL doc

--- spark-4.0/clickhouse-spark/src/main/scala/org/apache/spark/sql/clickhouse/ClickHouseSQLConf.scala
+++ spark-4.0/clickhouse-spark/src/main/scala/org/apache/spark/sql/clickhouse/ClickHouseSQLConf.scala
@@ -106,7 +106,7 @@ object ClickHouseSQLConf {
       .doc("When writing Distributed table, write local table instead of itself. " +
         "If `true`, ignore `spark.clickhouse.write.distributed.useClusterNodes`. " +
         "This bypasses ClickHouse's native routing, requiring Spark to evaluate the sharding key. " +
-        s"When using unsupported sharding expressions, set `${IGNORE_UNSUPPORTED_TRANSFORM.key}` " +
+        "When using unsupported sharding expressions, set `spark.clickhouse.ignoreUnsupportedTransform` " +
         "to `false` to prevent silent data distribution errors.")
       .version("0.1.0")
       .booleanConf
