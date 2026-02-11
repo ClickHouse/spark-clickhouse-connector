dd0922d5f9940c384bd5548a1070c1b2aa312eb7
veniciosAlcantara <venicios.oliveira@meliuz.com.br>
2026-02-11 09:20:30 -0300

    fix

--- spark-4.0/clickhouse-spark/src/main/scala/com/clickhouse/spark/ClickHouseCatalog.scala
+++ spark-4.0/clickhouse-spark/src/main/scala/com/clickhouse/spark/ClickHouseCatalog.scala
@@ -17,7 +17,9 @@ package com.clickhouse.spark
 import com.clickhouse.client.ClickHouseProtocol
 import com.clickhouse.spark.exception.CHClientException
 import org.apache.spark.sql.catalyst.analysis._
+import org.apache.spark.sql.catalyst.SQLConfHelper
 import org.apache.spark.sql.clickhouse.{ExprUtils, SchemaUtils}
+import org.apache.spark.sql.clickhouse.ClickHouseSQLConf.USE_NULLABLE_QUERY_SCHEMA
 import org.apache.spark.sql.connector.catalog._
 import org.apache.spark.sql.connector.catalog.functions.UnboundFunction
 import org.apache.spark.sql.connector.expressions.Transform
@@ -46,6 +48,7 @@ class ClickHouseCatalog extends TableCatalog
     with FunctionCatalog
     with ClickHouseHelper
     with SQLHelper
+    with SQLConfHelper
     with Logging {
 
   private var catalogName: String = _
@@ -105,6 +108,9 @@ class ClickHouseCatalog extends TableCatalog
   }
 
   override def name(): String = catalogName
+
+  // for SPARK-43390
+  override def useNullableQuerySchema: Boolean = conf.getConf(USE_NULLABLE_QUERY_SCHEMA)
 
   @throws[NoSuchNamespaceException]
   override def listTables(namespace: Array[String]): Array[Identifier] = namespace match {
